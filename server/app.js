e6614359bda336443b6b71d3caaaf1c0beb87b47
Ravindu <ravindumadawalagama@gmail.com>
2023-10-25 09:32:24 +0530

    Additional APIs

--- server/app.js
+++ server/app.js
@@ -12,9 +12,26 @@ const client = new Client({
     node: "https://localhost:9200"
 })
 
+
+app.get("/all-lines", async (req, res) => {
+    const result = await client.search({
+        index: "sinhala-metaphors",
+        body: {
+            "query": {
+                "match_all": {}
+            }
+        },
+    });
+
+    console.log(JSON.stringify(result))
+    res.json(result);
+});
+
+
 app.get("/all-metaphors", async (req, res) => {
     const result = await client.search({  
         index: "sinhala-metaphors",
+        size: 100,
         body: {      
             "query": {
                 "term": {
@@ -28,34 +45,72 @@ app.get("/all-metaphors", async (req, res) => {
     });
 
 app.post("/metaphors-by-poet", async (req, res) => {
-    const {poet} = req.body;
+    const poet = req.body.poet;
     const result = await client.search({
         index: "sinhala-metaphors",
         body: {
-        "query": {
-            "bool": {
-                "must": [
-                {
-                    "match": {
-                    "Poet": poet
+            "query": {
+                "bool": {
+                    "must": [
+                    {
+                        "match": {
+                        "Poet": poet
+                        }
+                    },
+                    {
+                        "match": {
+                        "Metaphor_present_or_not": "Yes"
+                        }
                     }
-                },
-                {
-                    "match": {
-                    "Metaphor_present_or_not": "Yes"
-                    }
+                    ]
                 }
-                ]
-            }
             }
         },
     });
 
-    console.log(result)
+    console.log(JSON.stringify(result))
     res.json(result);
 })
 
-app.post("/search-key", async (req, res) => {
+app.post("/all-by-poet", async (req, res) => {
+    const poet = req.body.poet;
+    const result = await client.search({
+        index: "sinhala-metaphors",
+        body: {
+            "query": {
+                "match": {
+                    "Poet": poet
+                }
+            } 
+        },
+    });
+
+    console.log(JSON.stringify(result))
+    res.json(result);
+})
+
+app.post("/search-time-period", async (req, res) => {
+    const start = req.body.start;
+    const end = req.body.end;
+    const result = await client.search({
+        index: "sinhala-metaphors",
+        body: {
+            "query": {
+                "range": {
+                "Year": {
+                    "gte": start,
+                    "lte": end
+                }
+                }
+            }                           
+        }
+    });
+
+    console.log(JSON.stringify(result))
+    res.json(result)
+});
+
+app.post("/search-all", async (req, res) => {
     const {word} = req.body;
     const result = await client.search({
         index: "sinhala-metaphors",
@@ -94,6 +149,112 @@ app.post("/search-key", async (req, res) => {
     res.json(result);
 });
 
+app.post("/search-all-metaphors", async (req, res) => {
+    const {word} = req.body;
+    const result = await client.search({
+        index: "sinhala-metaphors",
+        size: 100,
+        body :
+        {query: {
+          "bool": {
+            "must": [
+              {
+                "multi_match": {
+                  "query": word,
+                  "fields": ["Poem_Name", "Line"]
+                }
+              }
+            ],
+            "filter": [
+              {"term": {
+                "Metaphor_present_or_not": "yes"
+                }
+            }
+            ]
+          }
+        }
+    }
+    });
+
+    console.log(JSON.stringify(result))
+    res.json(result);
+});
+
+app.post("/search-all-metaphors-poet", async (req, res) => {
+    const {word, poet} = req.body;
+    const result = await client.search({
+        index: "sinhala-metaphors",
+        size: 100,
+        body :
+        {query: {
+          "bool": {
+            "must": [
+              {
+                "multi_match": {
+                  "query": word,
+                  "fields": ["Poem_Name", "Line"]
+                }
+              }
+            ],
+            "filter": [
+              {"match": {
+                "Poet": poet
+                }
+            }
+            ]
+          }
+        }
+    }
+    });
+
+    console.log(JSON.stringify(result))
+    res.json(result);
+});
+
+
+app.post("/search-poem", async (req, res) => {
+    const {word} = req.body;
+    const result = await client.search({
+        index: "sinhala-metaphors",
+        body: {
+            "query": {
+                "match": {
+                    "Peom_Name": word
+                }
+            } 
+        },
+    });
+
+    console.log(result)
+    res.json(result);
+});
+
+
+app.post("/search-advanced", async (req, res, next) => {
+    const {queryArgs} = req.body;
+
+    let queryArgKeys = Object.keys(queryArgs)    
+    let queries = []
+    
+    for (let i = 0; i < queryArgKeys.length; i++) {
+        queries.push({
+            match: { [queryArgKeys[i]]: { query: queryArgs[queryArgKeys[i]], operator: "AND" } },
+        })
+    }
+    const result = await client.search({
+        index: "sinhala-metaphors",
+        body: {
+            query: {
+                bool: {
+                    must:queries
+                }
+            }
+        }
+    });
+
+    console.log(result)
+    res.json(result);
+});
 
 
 app.listen(3001, () => console.log("App listening on http://localhost:3001"));
